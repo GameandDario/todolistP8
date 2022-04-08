fed3203bd98d8e52d8eebb4f7b19f2d5b9b12035
Stéphane <stephane.sokeo@gmail.com>
2022-04-08 14:18:39 +0200

    set jsdoc env

--- js/controller.js
+++ js/controller.js
@@ -92,7 +92,8 @@
    * An event to fire whenever you want to add an item. Simply pass in the event
    * object and it'll handle the DOM insertion and saving of the new item.
    */
-  /* ERREUR : Controller.prototype.adddItem = function (title) { */
+
+  /* ERREUR corrigée : Controller.prototype.adddItem = function (title) { */
   Controller.prototype.addItem = function (title) {
     var self = this;
 
